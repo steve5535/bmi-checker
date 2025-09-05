39fc0c0c145527940f7164fd6ba964e4d252144b
steve5535 <steve5535@naver.com>
2025-09-05 21:00:24 +0900

    feat: BMI지수 계산기 기능 추가

---
+++ bmi-checker.py
@@ -0,0 +1,12 @@
+weight=int(input('몸무게를 입력해주세요(kg) : '))
+height=int(input('키를 입력해주세요(cm) : '))
+bmi=round(weight/((height/100)**2),1)
+print(weight,height)
+if bmi < 18.5:
+    print(f'당신의 BMI(체질량)지수는 {bmi}이고 저체중 입니다.')
+elif 18.5 <= bmi < 22.9:
+    print(f'당신의 BMI(체질량)지수는 {bmi}이고 정상체중 입니다.')
+elif 23 < bmi < 24.9:
+    print(f'당신의 BMI(체질량)지수는 {bmi}이고 과체중 입니다.')
+else:
+        print(f'당신의 BMI(체질량)지수는 {bmi}이고 비만 입니다.')
